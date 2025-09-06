5fb88935563b6d622b1cebd2f44b2e03be88c899
tanvir-hadan-code <tanvirx015@gmail.com>
2025-09-06 22:48:42 +0600

    allVolunteer API Create

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// UserName:
+// Password:
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ot8ggjo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
+    await client.connect();
+    const postVolunteerCollections = client
+      .db("Volunteer-Management")
+      .collection("all-volunteerPost");
+
     app.get("/", (req, res) => {
       res.send("Welcome to Volunteer Management Server Root Page");
     });
+
+    app.get("/allVolunteerPosts", async (req, res) => {
+      const result = await postVolunteerCollections.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close()
   }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Volunteer Management app listening on port ${port}`);
