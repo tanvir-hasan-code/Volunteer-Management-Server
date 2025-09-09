98d529804711178e065963b857ef789af0c13c37
tanvir-hadan-code <tanvirx015@gmail.com>
2025-09-10 00:29:57 +0600

    Update API Create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
@@ -35,6 +35,70 @@ async function run() {
     app.get("/allVolunteerPosts", async (req, res) => {
       const result = await postVolunteerCollections.find().toArray();
       res.send(result);
+    });
+
+    app.get("/allVolunteerPosts/detailsPost/:id", async (req, res) => {
+      const id = req.params.id;
+      const quarry = { _id: new ObjectId(id) };
+
+      const result = await postVolunteerCollections.findOne(quarry);
+      res.send(result);
+    });
+
+	  app.post("/addVolunteerPost", async (req, res) => {
+		  const newNeedVolunteer = req.body;
+		  const result = await postVolunteerCollections.insertOne(newNeedVolunteer);
+		  res.send(result);
+    })
+    
+    app.get("/manageMyPost/myCreatedPosts", async (req, res) => {
+      const email = req.query.email;
+      const quarry = { post_owner: email }
+      
+      const result = await postVolunteerCollections.find(quarry).toArray();
+      res.send(result)
+
+    })
+
+    app.put("/allVolunteerPosts/detailsPost/:id", async (req, res) => {
+      const id  = req.params.id;
+      const updateData = req.body;
+      const quarry = { _id: new ObjectId(id) }
+      
+      const result = await postVolunteerCollections.updateOne(quarry, {
+        $set: updateData
+      })
+
+      res.send(result);
+
+    })
+
+    app.delete("/manageMyPost/:id", async (req, res) => {
+      const {id}  = req.params;
+      console.log(id)
+      const quarry = { _id: new ObjectId(id) };
+      const result = await postVolunteerCollections.deleteOne(quarry);
+     res.send(result)
+    })
+	  
+	  
+    app.get("/search", async (req, res) => {
+      const { q } = req.query;
+
+      try {
+        const results = await postVolunteerCollections
+          .find({
+            $or: [
+              { title: { $regex: q, $options: "i" } },
+            ],
+          })
+          .toArray();
+
+        res.json(results);
+      } catch (error) {
+        console.error("Search Error:", error);
+        res.status(500).json({ message: "Server Error" });
+      }
     });
 
     await client.db("admin").command({ ping: 1 });
